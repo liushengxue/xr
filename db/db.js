8e9692d44c9be12be3d87434aaf05d4e2001469a
ken <44867734+liushengxue@users.noreply.github.com>
2022-11-20 16:41:38 +0800

    update

--- db/db.js
+++ db/db.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql')
 
 const conn = mysql.createConnection({
-    host: '43.139.188.146', // 数据库地址
+    host: '127.0.0.1', // 数据库地址
     user: 'root', // 账号
-    password: 'Qweasd123!',
+    password: '123456',
     database: 'xr' // 数据库名称
 })
 
